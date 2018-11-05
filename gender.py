1b69ad72ecb1e8d25bf836a51ceafdda3ed76f96
aojiu <xieweiyao@gmail.com>
2018-11-05 13:58:11 -0500

    use train_test_split to split data and add cross validation

--- gender.py
+++ gender.py
@@ -10,20 +10,31 @@ from sklearn.metrics import accuracy_score
 url = "500_Person_Gender_Height_Weight_Index.csv"
 df = pd.read_csv(url, sep = ",")
 print(df.head(500))
-label = df.iloc[:, 0:1]
-label_train = label[0:400]
-label_test = label[401:500]
-print(label_test)
-feature = df.drop("Gender",axis = 1)
-feature_train = feature.loc[1:400,:]
 
-# print("aaaaa")
-# print(feature_train)
-feature_test = feature.loc[401:500,:]
-print(feature_test.head())
+array = df.values
+
+#split the data into test and train
+feature = array[:, 1:4]
+print(feature)
+label = array[:, 0:1]
+print(label)
+validation_size = 0.2
+feature_train, feature_test, label_train, label_test = model_selection.train_test_split(feature, label, test_size = validation_size, random_state = 42)
+
+
+#brutal force data split
+
+# label = df.iloc[:, 0:1]
+# label_train = label[0:400]
+# label_test = label[401:500]
+# print(label_test)
+# feature = df.drop("Gender",axis = 1)
+# feature_train = feature.loc[1:400,:]
+# feature_test = feature.loc[401:500,:]
+# print(feature_test.head())
 # print(df.head())
 
-#linear regression model
+#linear regression model  does not apply here unless i transform the label into float
 # regr = linear_model.LinearRegression()
 # regr.fit(feature_train, label_train)
 # predict_regr = regr.predict(feature_test)
@@ -33,8 +44,10 @@ cls = tree.DecisionTreeClassifier()
 cls.fit(feature_train, label_train)
 prediction_cls = cls.predict(feature_test)
 accuracy_cls = accuracy_score(prediction_cls, label_test)
-print(prediction_cls)
-print(accuracy_cls)
+cv_cls = model_selection.cross_val_score(cls, feature_train, label_train, cv = 10) #cv should be within the range of number of samples
+print("prediction for decision tree is:", prediction_cls)
+print("accuracy for decision tree is:", accuracy_cls)
+print("cross validation for decision tree is:", cv_cls)
 
 
 
